6f715e39546b2ea1bd07dc92da31c3b23f92ba62
MANJODH SARAN <manjodhsaran1@gmail.com>
2020-10-04 12:51:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
+import Feed from './Feed/Feed';
+import Widgets from './Widgets/Widgets';
+import Login from './Login/Login';
+import { useStateValue } from './StateProvider';
+
 function App() {
+
+  const [{ user }] = useStateValue();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      {!user ? <Login /> : (
+        <>
+          <Header />
+
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
+      <div className="app__footer">
+        <p>About • Help • Press • API • Jobs • Privacy • Terms • LocationsTop • Accounts • Hashtags • Language</p>
+        <h4>© 2020 FACEBOOK</h4>
+      </div>
     </div>
   );
 }
